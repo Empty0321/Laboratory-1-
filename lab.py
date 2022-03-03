068d6c515ffa735160e8f65628f416bb0e3dad7a
Empty0321 <100346307+Empty0321@users.noreply.github.com>
2022-03-03 17:29:21 +0800

    Update lab.py

--- lab.py
+++ lab.py
@@ -5,37 +5,4 @@
 \paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
 \pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
 
-\f0\fs24 \cf0 #!/usr/bin/env python3\
-# -*- coding: utf-8 -*-\
-# pylint: disable=C0103,C0111\
-\
-import argparse\
-\
-from snake.game import Game, GameConf, GameMode\
-\
-dict_solver = \{\
-    "greedy": "GreedySolver",\
-    "hamilton": "HamiltonSolver",\
-    "dqn": "DQNSolver",\
-\}\
-\
-dict_mode = \{\
-    "normal": GameMode.NORMAL,\
-    "bcmk": GameMode.BENCHMARK,\
-    "train_dqn": GameMode.TRAIN_DQN,\
-    "train_dqn_gui": GameMode.TRAIN_DQN_GUI,\
-\}\
-\
-parser = argparse.ArgumentParser(description="Run snake game agent.")\
-parser.add_argument("-s", default="hamilton", choices=dict_solver.keys(),\
-                    help="name of the solver to direct the snake (default: hamilton)")\
-parser.add_argument("-m", default="normal", choices=dict_mode.keys(),\
-                    help="game mode (default: normal)")\
-args = parser.parse_args()\
-\
-conf = GameConf()\
-conf.solver_name = dict_solver[args.s]\
-conf.mode = dict_mode[args.m]\
-print("Solver: %s    Mode: %s" % (conf.solver_name, conf.mode))\
-\
-Game(conf).run()}
+\f0\fs24 \cf0 print(\'91hello world\'92)}
